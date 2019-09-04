1fcb50cf7448df0b949a0f0d2489967c77a18ad9
KirinNg <kirinng0709@gmail.com>
2019-09-04 13:45:53 +0800

    JM

--- train_cifar_jiaming.py
+++ train_cifar_jiaming.py
@@ -1,6 +1,7 @@
 from utils import config, data_stream
 from utils import utils
 import os
+import tensorflow as tf
 
 os.environ["CUDA_VISIBLE_DEVISE"] = "0"
 
@@ -13,12 +14,30 @@ Cifar_datastream = data_stream.Cifar_datastream(Cifar_common_trainer.sess, Cifar
 Cifar_datastream_val = data_stream.Cifar_datastream(Cifar_common_trainer.sess, Cifar_train_config.BATCH_SIZE, "val")
 
 
+def StepLL_for_this_statu(x, logits, origin_label, eps=0.05):
+    total_class_num = tf.shape(logits)[1]
+    ori_class = tf.argmax(origin_label, 1, output_type=tf.int32)
+    one_hot_class = tf.one_hot((ori_class + 1)//total_class_num, total_class_num)
+    cross_entropy = tf.losses.softmax_cross_entropy(one_hot_class,
+                                                    logits,
+                                                    label_smoothing=0.1,
+                                                    weights=1.0)
+    x_adv = x - eps * tf.sign(tf.gradients(cross_entropy, x)[0])
+    x_adv = tf.clip_by_value(x_adv, -1.0, 1.0)
+    return tf.stop_gradient(x_adv)
 
 
+attack_op = StepLL_for_this_statu(Cifar_common_trainer.image_input_placehoder,
+                                  Cifar_common_trainer.logits,
+                                  Cifar_common_trainer.label_input_placehoder)
 
 for e in range(Cifar_train_config.epoch):
     for s in range(Cifar_train_config.step_per_epoch):
         image_batch, label_batch = Cifar_datastream.get_one_batch(Cifar_common_trainer.sess)
+
+        # get attacked img
+        image_batch = Cifar_common_trainer.sess.run(attack_op, feed_dict=Cifar_common_trainer.get_feed(image_batch, label_batch, "att"))
+
         _acc, cls_loss, l2 = Cifar_common_trainer.train(image_batch, label_batch)
         if (s + 1) % 20 == 0:
             val_image_batch, val_label_batch = Cifar_datastream_val.get_one_batch(Cifar_common_trainer.sess)
